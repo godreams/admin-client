3a3940a35fadbbbe7e6ea030be3fb82f9f6e0d01
Abdul Jaleel <jaleel@sv.co>
2017-02-08 00:05:53 +0530

    Update the DonationForm and make it pretty much functional

--- components/DonationForm.jsx
+++ components/DonationForm.jsx
@@ -8,16 +8,28 @@ import Heading from 'grommet/components/Heading'
 import FormField from 'grommet/components/FormField'
 import TextInput from 'grommet/components/TextInput'
 import NumberInput from 'grommet/components/NumberInput'
-import DateTime from 'grommet/components/DateTime'
 import Footer from 'grommet/components/Footer'
 import Button from 'grommet/components/Button'
+import ApiService from 'ApiService'
 
 @inject('appState') @observer
 export default class DonationForm extends React.Component {
+  @observable donationDetails = {
+    name: null,
+    email: null,
+    phone: null,
+    amount: null,
+    date: null,
+    pan: null,
+    address: null
+  }
+
   constructor (props) {
     super(props)
 
     this.handleClose = this.handleClose.bind(this)
+    this.updateDetail = this.updateDetail.bind(this)
+    this.onChange = this.onChange.bind(this)
     this.submit = this.submit.bind(this)
   }
 
@@ -25,8 +37,23 @@ export default class DonationForm extends React.Component {
     this.props.closeLayerCB()
   }
 
+  updateDetail (key, value) {
+    this.donationDetails[key] = value
+  }
+
+  onChange (event) {
+    this.updateDetail(event.target.name, event.target.value)
+  }
+
   submit () {
-    console.log('Submitted the new donation!')
+    let form = new FormData()
+    Object.keys(this.donationDetails).forEach(key => form.append(key, this.donationDetails[key]));
+
+    let apiService = new ApiService(this.props.appState.authorization.token)
+    apiService.post('donations', form).then(response => {
+      // TODO: Handle response
+      console.log(response)
+    })
   }
 
   render () {
@@ -35,17 +62,26 @@ export default class DonationForm extends React.Component {
         <Form pad='medium'>
           <Header>
             <Heading>
-              Donation Details
+              Donor Details
             </Heading>
           </Header>
-          <FormField label='Name of Donor'>
-            <TextInput />
+          <FormField label='Name'>
+            <TextInput name='name' onDOMChange={ this.onChange }/>
+          </FormField>
+          <FormField label='Email'>
+            <TextInput name='email' onDOMChange={ this.onChange }/>
+          </FormField>
+          <FormField label='Phone'>
+            <TextInput name='phone' onDOMChange={ this.onChange }/>
           </FormField>
           <FormField label='Amount'>
-            <NumberInput />
+            <NumberInput name='amount' onChange={ this.onChange }/>
           </FormField>
-          <FormField label='Date'>
-            <DateTime />
+          <FormField label='PAN'>
+            <TextInput name='pan' onDOMChange={ this.onChange }/>
+          </FormField>
+          <FormField label='Address'>
+            <TextInput name='address' onDOMChange={ this.onChange }/>
           </FormField>
           <Footer pad={{'vertical': 'medium'}}>
             <Button label='Add' type='submit' onClick={this.submit}/>
